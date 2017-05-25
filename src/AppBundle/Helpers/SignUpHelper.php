f37f58a779cc48f976bc6d986b5a9e2ac1998081
Basionkler <pergdr@yahoo.it>
2017-05-25 16:31:47 +0200

    Minor fix

--- src/AppBundle/Helpers/SignUpHelper.php
+++ src/AppBundle/Helpers/SignUpHelper.php
@@ -16,7 +16,7 @@ class SignUpHelper extends Controller {
         if(strlen(str_replace(" ", "", $user->getLastname())) < 3) return false;
         if(strlen(str_replace(" ", "", $user->getEmail())) < 3 && !strpos($user->getEmail(),"@") && !strpos($user->getEmail(),'.')) return false;
         if(strlen(str_replace(" ", "", $user->getNickname())) < 3) return false;
-        if(strlen($user->getPassword()<6)) return false;
+        if(strlen($user->getPassword()) < 6) return false;
         if(strpos($user->getName()," ")) return false;
         return true;
     }
@@ -26,7 +26,7 @@ class SignUpHelper extends Controller {
         if(strlen(str_replace(" ", "", $user->getLastname())) < 3) $form->get('lastname')->addError(new FormError("Insert a valid lastname."));
         if(strlen(str_replace(" ", "", $user->getEmail())) < 3 && !strpos($user->getEmail(),"@") && !strpos($user->getEmail(),'.')) $form->get('email')->addError(new FormError("Insert a valid email."));
         if(strlen(str_replace(" ", "", $user->getNickname())) < 3) $form->get('nickname')->addError(new FormError("Insert a valid nickname."));
-        if(strlen($user->getPassword()<6)) $form->get('password')->addError(new FormError("Your password has to be at least 6 characters."));
+        if(strlen($user->getPassword()) < 6) $form->get('password')->addError(new FormError("Your password has to be at least 6 characters."));
         if(strpos($user->getPassword()," ")) $form->get('password')->addError(new FormError("Insert a valid name."));
         return $form;
     }
